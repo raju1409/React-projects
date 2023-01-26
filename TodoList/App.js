4fdba651a140799322ee2e4e299c48592d758994
RAJ <107236972+raju1409@users.noreply.github.com>
2023-01-26 17:35:20 +0530

    Update App.js

--- TodoList/App.js
+++ TodoList/App.js
@@ -4,21 +4,40 @@ import { useState } from 'react';
 function App() {
   const [todoList, setTodolist] = useState([]);
   const [newTask, setNewTask] = useState("");
+  
   const handleTask = (event)=>{
     setNewTask(event.target.value);
-    console.log(newTask)
   }
   const handleTodolist = ()=>{
-    const TaskList = [...todoList, newTask];
+    let task = {
+      id : todoList.length ===0?1: todoList[todoList.length-1].id+1,
+      taskName1 : newTask
+    }
+    const TaskList = [...todoList, task];
     setTodolist(TaskList)
+    setNewTask("")
   }
+  const deleteTask = (taskId)=>{
+    const deleteList = todoList.filter(task => {
+      // return taskId !== task.id ONELINER
+      if (taskId === task.id ){
+        return false
+      }else{
+        return true
+      }
+    })
+    setTodolist(deleteList)
+  }
+// with value={newTask} the placehoder value will be ""
   return <div className={styles.App}>
-    <input onChange={handleTask}/>
+    <input value={newTask} onChange={handleTask} />
     <button onClick={handleTodolist}> Add Task</button>
-    {newTask}
     <div className='todolist'>
       {todoList.map(task=>{
-        return <h2>{task}</h2>
+        return <div>
+          <h2 >{task.taskName1}--{task.id}</h2>
+          <button onClick={()=>deleteTask(task.id)}>X</button>
+        </div>
       })}
     </div>
     </div>
