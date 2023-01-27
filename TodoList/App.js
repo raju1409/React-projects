f6f9c1c85f26ca145e14ccdc957cd9ef0f8aead4
RAJ <107236972+raju1409@users.noreply.github.com>
2023-01-27 11:06:39 +0530

    Update App.js
    second commit

--- TodoList/App.js
+++ TodoList/App.js
@@ -1,45 +1,55 @@
 import styles from './App.module.css';
 import { useState } from 'react';
+import {Todo} from "./Todolist"
 
 function App() {
-  const [todoList, setTodolist] = useState([]);
-  const [newTask, setNewTask] = useState("");
-  
-  const handleTask = (event)=>{
-    setNewTask(event.target.value);
+  const [TodoList, setTodoList] = useState([]);
+  const [newTask, setnewTask] = useState("");
+
+  const handleNewTask = (event)=>{
+
+    setnewTask(event.target.value)
   }
-  const handleTodolist = ()=>{
-    let task = {
-      id : todoList.length ===0?1: todoList[todoList.length-1].id+1,
-      taskName1 : newTask
+  const handleTodoList = ()=>{
+    const Task = {
+      id : TodoList.length===0 ? 1 : TodoList[TodoList.length-1].id+1,
+      Name : newTask,
+      completeTask : false
     }
-    const TaskList = [...todoList, task];
-    setTodolist(TaskList)
-    setNewTask("")
+    const arr = [...TodoList, Task]
+    setTodoList(arr)
+    setnewTask("")
+
   }
-  const deleteTask = (taskId)=>{
-    const deleteList = todoList.filter(task => {
-      // return taskId !== task.id ONELINER
-      if (taskId === task.id ){
-        return false
+  const deleteTask = (id)=>{
+    const newList = TodoList.filter((task)=>task.id!==id)
+    setTodoList(newList)
+  }
+  const handlecompleteTask = (id)=>{
+    setTodoList(TodoList.map((task)=>{
+      if (task.id === id){
+        return {...task, completeTask : true};
       }else{
-        return true
+        return task;
       }
-    })
-    setTodolist(deleteList)
+    }))
+    
+
   }
-// with value={newTask} the placehoder value will be ""
+ 
   return <div className={styles.App}>
-    <input value={newTask} onChange={handleTask} />
-    <button onClick={handleTodolist}> Add Task</button>
-    <div className='todolist'>
-      {todoList.map(task=>{
-        return <div>
-          <h2 >{task.taskName1}--{task.id}</h2>
-          <button onClick={()=>deleteTask(task.id)}>X</button>
-        </div>
-      })}
+    <div className={styles.nav}>
+    <h1>Todo List</h1>
+    <input value={newTask} onChange={handleNewTask} className={styles.input}/>
+    <button onClick={()=>{handleTodoList()}}> Add Task </button>
     </div>
+      {TodoList.map(task =>{
+        return <Todo
+        completeTask={task.completeTask} 
+        Name={task.Name} id={task.id} 
+        deleteTask={deleteTask} 
+        handlecompleteTask={handlecompleteTask}/>
+      })}
     </div>
 }
 export default App;
